a9a527d6fc23fe4b775d8241d2c47347d93674d1
Katarzyna Fronc <katarzynaanna.fronc@gmail.com>
2024-01-11 15:46:23 +0100

    fix web css

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -16,6 +16,8 @@ export interface variantProps {
   image: string;
   name: string;
   price: string;
+  amount: string;
+  currency: string;
   period: string;
   space: string;
   domain: string;
@@ -87,14 +89,105 @@ export const variants = [
 
 const variantsContainer = document.getElementById("variantsContainer");
 
-if (variantsContainer) {
-  variants.forEach((variant, index) => {
+// for (let variant of variants) {
+//   let price = variant.price;
+//   let amount = price.slice(0, -2);
+//   let currency = price.slice(-2);
+// }
+
+// if (variantsContainer) {
+//   variants.forEach((variant, index) => {
+//     const newElement = document.createElement("div");
+//     const imgElement = document.createElement("img");
+//     const nameElement = document.createElement("h4");
+//     const lineElement = document.createElement("hr");
+//     const costElement = document.createElement("div");
+//     const amountElement = document.createElement("span");
+//     const currencyElement = document.createElement("sup");
+//     const periodElement = document.createElement("p");
+//     const spaceElement = document.createElement("p");
+//     const domainElement = document.createElement("p");
+//     const discElement = document.createElement("p");
+//     const offersElement = document.createElement("p");
+//     const supportElement = document.createElement("p");
+//     const buttonElement = document.createElement("button");
+
+//     imgElement.src = variant.image;
+//     nameElement.textContent = variant.name;
+
+//     amountElement.textContent = amount;
+//     currencyElement.textContent = currency;
+//     periodElement.textContent = variant.period;
+//     spaceElement.textContent = variant.space;
+//     domainElement.textContent = variant.domain;
+//     discElement.textContent = variant.disc;
+//     offersElement.textContent = variant.offers;
+//     supportElement.textContent = variant.support;
+
+//     buttonElement.textContent = "Zobacz więcej";
+
+//     newElement.classList.add("section__variants-card");
+//     imgElement.classList.add("section__variants-img");
+//     nameElement.classList.add("section__variants-name");
+//     lineElement.classList.add("section__variants-line");
+//     costElement.classList.add("section__variants-cost");
+//     amountElement.classList.add("section__variants-amount");
+//     currencyElement.classList.add("section__variants-currency");
+//     periodElement.classList.add("section__variants-period");
+//     spaceElement.classList.add("section__variants-card-p");
+//     domainElement.classList.add("section__variants-card-p");
+//     discElement.classList.add("section__variants-card-p");
+//     offersElement.classList.add("section__variants-card-p");
+//     supportElement.classList.add("section__variants-card-p");
+//     buttonElement.classList.add("section__variants-button");
+
+//     if (window.innerWidth <= 960 && index !== variants.length - 1) {
+//       newElement.appendChild(imgElement);
+//       newElement.appendChild(nameElement);
+//       newElement.appendChild(buttonElement);
+//       newElement.appendChild(lineElement);
+//       variantsContainer.appendChild(newElement);
+//     } else if (window.innerWidth <= 960) {
+//       newElement.appendChild(imgElement);
+//       newElement.appendChild(nameElement);
+//       newElement.appendChild(buttonElement);
+//       variantsContainer.appendChild(newElement);
+//     } else {
+//       newElement.appendChild(imgElement);
+//       newElement.appendChild(nameElement);
+//       newElement.appendChild(lineElement);
+//       newElement.appendChild(costElement);
+//       newElement.appendChild(amountElement);
+//       newElement.appendChild(currencyElement);
+//       costElement.appendChild(periodElement);
+//       newElement.appendChild(spaceElement);
+//       newElement.appendChild(domainElement);
+//       newElement.appendChild(discElement);
+//       newElement.appendChild(offersElement);
+//       newElement.appendChild(supportElement);
+//       newElement.appendChild(buttonElement);
+//       variantsContainer.appendChild(newElement);
+//     }
+//   });
+// } else {
+//   console.error("variantsContainer is null");
+// }
+
+// for (let variant of variants) {
+
+variants.forEach((variant, index) => {
+  let price = variant.price;
+  let amount = price.slice(0, -2);
+  let currency = price.slice(-2);
+
+  if (variantsContainer) {
     const newElement = document.createElement("div");
     const imgElement = document.createElement("img");
     const nameElement = document.createElement("h4");
     const lineElement = document.createElement("hr");
     const costElement = document.createElement("div");
-    const priceElement = document.createElement("span");
+    const amountElement = document.createElement("span");
+    const currencyElement = document.createElement("sup");
     const periodElement = document.createElement("p");
     const spaceElement = document.createElement("p");
     const domainElement = document.createElement("p");
@@ -105,8 +198,8 @@ if (variantsContainer) {
 
     imgElement.src = variant.image;
     nameElement.textContent = variant.name;
-
-    priceElement.textContent = variant.price;
+    amountElement.textContent = amount;
+    currencyElement.textContent = currency;
     periodElement.textContent = variant.period;
     spaceElement.textContent = variant.space;
     domainElement.textContent = variant.domain;
@@ -121,7 +214,8 @@ if (variantsContainer) {
     nameElement.classList.add("section__variants-name");
     lineElement.classList.add("section__variants-line");
     costElement.classList.add("section__variants-cost");
-    priceElement.classList.add("section__variants-price");
+    amountElement.classList.add("section__variants-amount");
+    currencyElement.classList.add("section__variants-currency");
     periodElement.classList.add("section__variants-period");
     spaceElement.classList.add("section__variants-card-p");
     domainElement.classList.add("section__variants-card-p");
@@ -146,7 +240,8 @@ if (variantsContainer) {
       newElement.appendChild(nameElement);
       newElement.appendChild(lineElement);
       newElement.appendChild(costElement);
-      costElement.appendChild(priceElement);
+      costElement.appendChild(amountElement);
+      costElement.appendChild(currencyElement);
       costElement.appendChild(periodElement);
       newElement.appendChild(spaceElement);
       newElement.appendChild(domainElement);
@@ -156,10 +251,10 @@ if (variantsContainer) {
       newElement.appendChild(buttonElement);
       variantsContainer.appendChild(newElement);
     }
-  });
-} else {
-  console.error("variantsContainer is null");
-}
+  } else {
+    console.error("variantsContainer is null");
+  }
+});
 
 // ----- slider -----
 
